ba6a44ea3f70ab030174e39a267274c22ba9088b
Ákos Klobetz <klobetz@gmail.com>
2024-03-13 17:32:25 +0100

    #6 alap form létrehozása és a modell elkészítése

---
+++ BlazorApp_Urlapok/BlazorApp_urlap/Components/Modell/CustomerItem.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp_urlap.Components.Modell
+{
+    public class CustomerItem
+    {
+        public int Id { get; set; }
+        public string? FName { get; set; }
+        public string? LName { get; set; }
+        public string? EMail { get; set; }
+        public string? PhoneNmber { get; set; }
+        public DateTime BirthDay { get; set; } = new DateTime(2024,01,01);
+        public string? Country { get; set; }
+        public string? City  { get; set; }
+        public int Price { get; set; }
+
+
+
+    }
+}
