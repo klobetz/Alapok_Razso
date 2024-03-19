4a1b7107ca970417160202accb23bd7bedfcb5ad
Ákos Klobetz <klobetz@gmail.com>
2024-03-19 17:41:11 +0100

    #6 validálási attribútumok beírása a modellbe

--- BlazorApp_Urlapok/BlazorApp_urlap/Components/Modell/CustomerItem.cs
+++ BlazorApp_Urlapok/BlazorApp_urlap/Components/Modell/CustomerItem.cs
@@ -9,12 +9,30 @@ namespace BlazorApp_urlap.Components.Modell
 
         [Required (ErrorMessage = "A mezőt kötelező kitölteni")]
         public string? FName { get; set; }
+
+        [Required]
         public string? LName { get; set; }
+
+        [Required]
+        [EmailAddress (ErrorMessage ="Nem megfelelő e-mail cím!")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$", ErrorMessage = "Érvénytelen e-mail formátum.")]
         public string? EMail { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength =3, ErrorMessage ="A mező hossza minimum 3 karakter és maximum 12 karaktert tartalmazhet.")]
         public string? PhoneNmber { get; set; }
-        public DateTime BirthDay { get; set; } = new DateTime(2024,01,01);
+
+        [Required]
+        [Range(typeof(DateTime),"1920-01-01","2024-01-01",ErrorMessage ="A dátum 1920.01.01-2024.01.01 -ig adható meg")]
+        public DateTime BirthDay { get; set; } = new DateTime(2000,01,01); //DateTime.Now;
+
         public string? Country { get; set; }
+
         public string? City  { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage ="Az árat 0 és 100 között határozhatod meg")]
         public int Price { get; set; }
+
     }
 }
