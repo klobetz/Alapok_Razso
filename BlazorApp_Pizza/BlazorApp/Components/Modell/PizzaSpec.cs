b88cda83b9587131fc9569712d2fa7c6e6212ba6
klobetz <klobetz@gmail.com>
2024-03-20 18:42:59 +0100

    #7 modell létrehozása

---
+++ BlazorApp_Pizza/BlazorApp/Components/Modell/PizzaSpec.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Components.Modell
+{
+    public class PizzaSpec
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal BasePrice { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string GetFormetBasePrice() => BasePrice.ToString("0.00");
+    }
+}
